f3fff138e4f8b22223268a696e77d3e9da0ae606
Lewis Andrew Campbell <lewis.andrew.campbell@gmail.com>
2016-02-03 13:45:07 +1300

    recording the types when parsing. untested
    Signed-off-by: Lewis Andrew Campbell <lewis.andrew.campbell@gmail.com>

--- Runtime.ml
+++ Runtime.ml
@@ -5,21 +5,26 @@
 
 open Core.Std
 
+type metadata = { addr: int; signature: Type.signature option }
+
 type t = {
   mutable free_addr: int;
-  symbol_table: (string, int) Hashtbl.t;
+  symbol_table: (string, metadata) Hashtbl.t
 }
 
 let create () = {
-  free_addr = -1;
-  symbol_table = String.Table.create ();
+  free_addr     = -1;
+  symbol_table  = String.Table.create ();
 }
 
 let whitespace = Str.regexp "[ \t\r\n]+\\|(\\|)"
+
 let tokenize = Str.split_delim whitespace
 
+let metadata_of_symbol rt = Hashtbl.find rt.symbol_table
+
 let addr_of_symbol rt sym = 
-  Hashtbl.find rt.symbol_table sym |> Option.map ~f:(fun addr -> IR.Call(addr))
+  metadata_of_symbol rt sym |> Option.map ~f:(fun md -> IR.Call(md.addr))
 
 let read_i32 s = Option.try_with (fun () -> (IR.Push(Int32.of_string s)))
 
@@ -29,7 +34,7 @@ let read_bool = function
   | _       -> None
 
 let primitives = String.Map.of_alist_exn
-  [";",           IR.DefEnd;
+  [";",             IR.DefEnd;
    "drop",          IR.Drop;
    "dup",           IR.Dup;
    "swap",          IR.Swap;
@@ -50,17 +55,22 @@ let primitives = String.Map.of_alist_exn
    "push",          IR.DaPush;
    "pop",           IR.DaPop]
 
-let lookup rt token =
+let lookup rt token : (IR.t * Type.signature) option =
   [Map.find primitives; read_i32; read_bool; addr_of_symbol rt]
   |> List.find_map ~f:(fun f -> f token)
+  |> Option.map ~f:(fun ir -> (ir, IR.to_type ir))
 
-let bind_sym rt sym =
+let bind_sym rt (sym, sig_) =
   rt.free_addr <- rt.free_addr + 1;
-  Hashtbl.add_exn rt.symbol_table ~key:sym ~data:rt.free_addr;
-  IR.DefBgn(rt.free_addr)
+  Hashtbl.add_exn 
+    rt.symbol_table 
+    ~key:sym 
+    ~data:{ addr = rt.free_addr; signature = sig_ }
 
 let parse_token rt token = match lookup rt token with
-  | Some ir -> ir
-  | None -> bind_sym rt token
- 
+  | Some (ir, _) -> ir
+  | None -> 
+      bind_sym rt (token, None); 
+      IR.DefBgn rt.free_addr
+
 let compile_to_ir rt str = tokenize str |> List.map ~f:(parse_token rt)
